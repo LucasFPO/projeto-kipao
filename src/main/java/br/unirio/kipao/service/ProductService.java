0995ed41f411d45f0de4c6818d8acfa9acef61a0
Cecilia Araújo <scecilia028@gmail.com>
2022-08-01 10:18:23 -0300

    update ProductService

--- src/main/java/br/unirio/kipao/service/ProductService.java
+++ src/main/java/br/unirio/kipao/service/ProductService.java
@@ -1,27 +1,39 @@
 package br.unirio.kipao.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.*;
 import org.springframework.stereotype.Service;
 
 import br.unirio.kipao.model.Product;
 import br.unirio.kipao.repository.ProductRepository;
 
+@CacheConfig(cacheNames = "product")
 @Service
 public class ProductService {
 	
 	@Autowired
 	private ProductRepository productRepository;
 
+	public ProductService() {
+	}	
 	public Product getProduct(String nameProduct) {
 		return productRepository.findByName(nameProduct);
 	}
-
+	
+	@Caching(evict = {@CacheEvict(value = "allproductcache", allEntries = true),
+	        @CacheEvict(value = "productcache") })
 	public Product saveProduct(@Valid Product productParam) {
 		return productRepository.save(productParam);
+	}
+	
+	@Cacheable(value = "allproductcache")
+	public List<Product> getAllProducts() {
+	    return (List<Product>) productRepository.findAll();
 	}
  
 }
